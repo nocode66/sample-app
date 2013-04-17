ca3d6a1006a9dd53cff66498093b0ff5fd13cb88
nocode66 <georgescu.cristi@gmail.com>
2013-04-17 13:24:50 +0300

    Added git conf and gems

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SampleApp::Application.config.secret_key_base = 'c6d180dd6e027b61b0da0217ea4cb7720c390925ebf10dc81df5e6681781d8d605406a4fe2fd65c4a26b5f468bcd3c5e9e426c3235f10d4ca58e7003b32f1e2a'
